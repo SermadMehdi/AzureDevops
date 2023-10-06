c082754348f7f5ea605d06bb7bb1061400583621
Sermad Mehdi <98916275+SermadMehdi@users.noreply.github.com>
2023-10-07 00:09:11 +0500

    Update and rename test.py to app.py

--- app.py
+++ app.py
@@ -9,6 +9,3 @@ for i in range(1, 6):
     time.sleep(1)  # Simulate some work
     print(f"Action {i} completed.")
     
-# Generate some log entries
-for log_entry in ["Log entry 1", "Log entry 2", "Log entry 3"]:
-    print(log_entry)
