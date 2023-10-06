cd99743b2f1a485ad039979b2abf5a97c3b911e6
Sermad Mehdi <98916275+SermadMehdi@users.noreply.github.com>
2023-10-07 00:38:18 +0500

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ print("Hello, World from Container trigger!")
 
 # Simulate some actions and log entries
 for i in range(1, 2):
-    print(f"Performing action {j}...")
+    print(f"Performing action {i}...")
     time.sleep(1)  # Simulate some work
-    print(f"Action {j} completed.")
+    print(f"Action {i} completed.")
     
