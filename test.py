acadd9ec832af54d594b8828f1296ea1046421eb
Sermad Mehdi <98916275+SermadMehdi@users.noreply.github.com>
2023-10-06 23:53:19 +0500

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,14 @@
-# Print "Hello, World!" to the console
+import time
+
+# Print a message to the console
 print("Hello, World from Container trigger!")
 
-
+# Simulate some actions and log entries
+for i in range(1, 6):
+    print(f"Performing action {i}...")
+    time.sleep(1)  # Simulate some work
+    print(f"Action {i} completed.")
+    
+# Generate some log entries
+for log_entry in ["Log entry 1", "Log entry 2", "Log entry 3"]:
+    print(log_entry)
